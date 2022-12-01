87da2498bd442f06f4c5135fc4ff57e5d42313aa
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2022-12-01 06:11:03 +0100

    Day 1

--- days/day01.py
+++ days/day01.py
@@ -2,7 +2,7 @@ from lib import load_input
 
 
 def solve(data, part=2):
-    lines = data.splitlines()
+    lines = data.strip().split("\n\n")
     if part == 1:
         return part_one(lines)
     elif part == 2:
@@ -10,13 +10,15 @@ def solve(data, part=2):
 
 
 def part_one(data):
-    pass
+    totals = [sum([int(n) for n in line.split("\n")]) for line in data]
+    return max(totals)
 
 
 def part_two(data):
-    pass
+    totals = [sum([int(n) for n in line.split("\n")]) for line in data]
+    return sum(sorted(totals, reverse=True)[:3])
 
 
 if __name__ == "__main__":
-    print(solve(load_input("small"), 1))
-    print(solve(load_input("small")))
+    print(solve(load_input(), 1))
+    print(solve(load_input()))
