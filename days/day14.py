87b1eab614cff05ee8af6ac250d7e8de25d77ea5
Duuk Niemantsverdriet <D.P.Niemantsverdriet@student.tudelft.nl>
2022-12-14 20:34:01 +0100

    Day 14 cleanup

--- days/day14.py
+++ days/day14.py
@@ -29,42 +29,40 @@ def parse_rocks(data):
 def part_one(data):
     total = 0
     max_y = max(map(lambda x: x[1], data))
+    sand = (500, 0)
     while True:
-        sand = (500, 0)
-        while True:
-            if sand[1] > max_y:
-                return total
-            if (sand[0], sand[1] + 1) not in data:
-                sand = (sand[0], sand[1] + 1)
-            elif (sand[0] - 1, sand[1] + 1) not in data:
-                sand = (sand[0] - 1, sand[1] + 1)
-            elif (sand[0] + 1, sand[1] + 1) not in data:
-                sand = (sand[0] + 1, sand[1] + 1)
-            else:
-                total += 1
-                data.add(sand)
-                sand = (500, 0)
+        if sand[1] > max_y:
+            return total
+        if (sand[0], sand[1] + 1) not in data:
+            sand = (sand[0], sand[1] + 1)
+        elif (sand[0] - 1, sand[1] + 1) not in data:
+            sand = (sand[0] - 1, sand[1] + 1)
+        elif (sand[0] + 1, sand[1] + 1) not in data:
+            sand = (sand[0] + 1, sand[1] + 1)
+        else:
+            total += 1
+            data.add(sand)
+            sand = (500, 0)
 
 
 def part_two(data):
     total = 0
     floor = max(map(lambda x: x[1], data)) + 2
+    sand = (500, 0)
     while True:
-        sand = (500, 0)
-        while True:
-            on_floor = sand[1] == floor - 1
-            if (sand[0], sand[1] + 1) not in data and not on_floor:
-                sand = (sand[0], sand[1] + 1)
-            elif (sand[0] - 1, sand[1] + 1) not in data and not on_floor:
-                sand = (sand[0] - 1, sand[1] + 1)
-            elif (sand[0] + 1, sand[1] + 1) not in data and not on_floor:
-                sand = (sand[0] + 1, sand[1] + 1)
-            else:
-                total += 1
-                if sand == (500, 0):
-                    return total
-                data.add(sand)
-                sand = (500, 0)
+        on_floor = sand[1] == floor - 1
+        if (sand[0], sand[1] + 1) not in data and not on_floor:
+            sand = (sand[0], sand[1] + 1)
+        elif (sand[0] - 1, sand[1] + 1) not in data and not on_floor:
+            sand = (sand[0] - 1, sand[1] + 1)
+        elif (sand[0] + 1, sand[1] + 1) not in data and not on_floor:
+            sand = (sand[0] + 1, sand[1] + 1)
+        else:
+            total += 1
+            if sand == (500, 0):
+                return total
+            data.add(sand)
+            sand = (500, 0)
 
 
 if __name__ == "__main__":
